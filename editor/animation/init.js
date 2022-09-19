0c847552800e5bf91ce374f79522e0385d79c42e
Oleksandr Zozulia <freemanlex@gmail.com>
2022-09-19 14:56:16 +0300

    next-api uk uk-hints

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,16 +1,6 @@
-//Dont change it
-//Dont change it
-requirejs(['ext_editor_io', 'jquery_190'],
+requirejs(['ext_editor_io2', 'jquery_190'],
     function (extIO, $) {
-        
-        var $tryit;
-
-        var io = new extIO({
-            functions: {
-                python: 'easy_unpack',
-                js: 'easyUnpack'
-            }
-        });
+        var io = new extIO({});
         io.start();
     }
 );
